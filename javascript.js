1cc94600037d8c5ce0a432cda50bb97aeac329e4
tristanhache <120027436+tristanhache@users.noreply.github.com>
2023-02-05 14:08:04 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,30 @@
-let count = 0;
-function myFunction() {
-  count+=1;
-  document.getElementById("btn").innerHTML = count;
+var clicks = 0;
 
- }
+function onClick() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+};
+
+
+ function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 1,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
